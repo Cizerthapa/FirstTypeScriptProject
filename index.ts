58fc8a26c37dbcffa3e4157f33df69ac61075e6a
Cizerthapa <cizerthapa@gmail.com>
2024-08-23 21:53:05 +0545

    CRD Done

--- index.ts
+++ index.ts
@@ -28,6 +28,68 @@ app.get('/data', async (req, res) => {
     }
 });
 
+app.post('/data', async (req, res) => {
+    try {
+        let username: string = req.body.username;
+        let password: string = req.body.password;
+        let role: string = req.body.role;
+        let fullname: CharacterData = req.body.fullname;
+        if (!username) {
+            return res.status(400).send('username is required');
+        }
+        if (!password) {
+            return res.status(400).send('password is required');
+        }
+        
+        if(fullname.length < 2){
+            return res.status(400).send('fullname must be grater than 2 letters');
+        }
+
+        // Check if the username already exists
+        const checkQuery = `SELECT * FROM users WHERE username = $1`;
+        const checkResult = await client.query(checkQuery, [username]);
+
+        if (checkResult.rows.length > 0) {
+            return res.status(400).send('Username already exists. Please choose a different one.');
+        }
+        if (role == 'a' || role == 'u') {
+            const query = `INSERT INTO users (username, password, role, fullname) VALUES ($1, $2, $3, $4) RETURNING *`;
+            const values = [username, password, role, fullname];
+
+            const result = await client.query(query, values);
+            return res.status(200).send("OK!" + result);
+        } else {
+            return res.status(400).send('role is required in "u" for user or "a" for admin');
+        }
+    } catch (err) {
+        console.error('Query Error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.delete('/data', async (req, res) => {
+    try {
+        const username: string = req.body.username;
+
+        // Check if the username already exists
+        const checkQuery = `SELECT * FROM users WHERE username = $1`;
+        const checkResult = await client.query(checkQuery, [username]);
+
+        if (checkResult.rows.length > 0) {
+            const query = `DELETE FROM users WHERE username = ($1) RETURNING *`;
+
+            const result = await client.query(query, [username]);
+            res.json(result.rows[0]);
+            return res.status(200).send("OK!");
+        } else {
+            return res.status(400).send('No user found');
+        }
+    } catch (err) {
+        console.error('Query Error', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 app.listen(PORTEX, () =>
     console.log(`your server is running on port ${PORTEX}`)
 );
